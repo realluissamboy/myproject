04e4e535a0e352590f4777522800289ccebbd30c
Luis Samboy <luissamboy@icloud.com>
2023-10-05 16:57:30 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hola Mundo</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-project';
+  title = 'portfolio';
 }
